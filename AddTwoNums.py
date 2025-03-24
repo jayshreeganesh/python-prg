cdb97acd7abbc215041c4cec0c95e61f5864faf6
Ganesh Kale <jayshreeganesh333@gmail.com>
2025-03-24 20:56:29 +0530

    Add Two Numbers

---
+++ AddTwoNums.py
@@ -0,0 +1,18 @@
+#Solution-1 (with pre-defined variables)
+
+num1 = 30
+num2 = 40
+num = num1+num2
+
+print("The sum of given two numbers is ", num1+num2)
+
+print("The sum of given two numbers is ", num)
+
+#Solution-2 (with user-inputs)
+
+num3 = float(input("Enter a number here: "))
+num4 = float(input("Enter another number here: "))
+
+num_sum = num3 + num4
+
+print("The sum of the provided two numbers is: ", num_sum)
