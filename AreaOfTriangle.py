ce77cb8a8b895ded0c66417e67a0c92b8e01dd14
Ganesh Kale <jayshreeganesh333@gmail.com>
2025-03-24 21:35:37 +0530

    Calculate Area of Triangle

---
+++ AreaOfTriangle.py
@@ -0,0 +1,8 @@
+height = float(input("Enter the height of the tringle "))
+base = float(input("Enter the base of the tringle "))
+
+area = (1/2)*height*base
+area2 = (0.5)*height*base
+
+print("The area of triagle is ", area)
+print("The area of triagle is ", area2)
