45fc30cbf6831e251ec68a3a6f89e4f54c477d2d
Ganesh Kale <jayshreeganesh333@gmail.com>
2025-03-27 16:33:57 +0530

    Is a Number Positive Negative or Zero?

---
+++ PosNegZero.py
@@ -0,0 +1,15 @@
+# Program to check a number is positive, negative or zero
+
+# num = 5
+# num = -5
+# num = 0
+
+num = int(input("Enter a number here : "))
+
+if num > 0 :
+    print("It is positive number")
+elif num == 0 :
+    print("It is zero")
+else :
+    print("It is negative number")
+
