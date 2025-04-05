b9b2ac2cee6d8f5dff954bd9fb48b0d3c9f49a33
Ganesh Kale <jayshreeganesh333@gmail.com>
2025-04-05 21:08:56 +0530

    Display the Multiplication Table

--- MultiplicationTable.py
+++ MultiplicationTable.py
@@ -5,7 +5,7 @@ n = int(input("Enter a number here : "))
 for i in range(1,13):
     print(n,"x",i,"=",n*i)
 
-# Solution 2 with while loop
+# Solution 2 - with while loop
 
 m = int(input("Enter a number here : "))
 j = 0
